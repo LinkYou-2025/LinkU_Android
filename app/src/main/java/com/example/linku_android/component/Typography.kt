27f7d8fe5cbe5dbbc63497683d323f068b215d79
potato <katete051@naver.com>
2025-07-10 21:18:06 +0900

    폰트 추가, 이메일 인증, 흥미 목적, 로그인 스크린 폰트 적용 완료, (나머지는 추후 폰트), 로그인 약간 동의 만들어야 함

---
+++ app/src/main/java/com/example/linku_android/component/Typography.kt
@@ -0,0 +1,18 @@
+package com.example.linku_android.component
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.example.linku_android.R
+
+val Paperlogy = FontFamily(
+    Font(R.font.paperlogy_1thin, FontWeight.Thin),
+    Font(R.font.paperlogy_2extralight, FontWeight.ExtraLight),
+    Font(R.font.paperlogy_3light, FontWeight.Light),
+    Font(R.font.paperlogy_4regular, FontWeight.Normal),
+    Font(R.font.paperlogy_5medium, FontWeight.Medium),
+    Font(R.font.paperlogy_6semibold, FontWeight.SemiBold),
+    Font(R.font.paperlogy_7bold, FontWeight.Bold),
+    Font(R.font.paperlogy_8extrabold, FontWeight.ExtraBold),
+    Font(R.font.paperlogy_9black, FontWeight.Black)
+)
