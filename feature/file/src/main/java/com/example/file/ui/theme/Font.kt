a8b4601ef760165418db5458393c4dc28aeb16ec
gmin <gllom7884@gmail.com>
2025-07-12 22:17:26 +0900

    ✨ 폰트 추가

---
+++ feature/file/src/main/java/com/example/file/ui/theme/Font.kt
@@ -0,0 +1,18 @@
+package com.example.file.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import com.example.file.R
+
+val DefaultFont = FontFamily(
+    Font(R.font.paperlogy_black, FontWeight.Black, FontStyle.Normal),
+    Font(R.font.paperlogy_bold, FontWeight.Bold, FontStyle.Normal),
+    Font(R.font.paperlogy_extra_bold, FontWeight.ExtraBold, FontStyle.Normal),
+    Font(R.font.paperlogy_light, FontWeight.Light, FontStyle.Normal),
+    Font(R.font.paperlogy_medium, FontWeight.Medium, FontStyle.Normal),
+    Font(R.font.paperlogy_regular, FontWeight.Normal, FontStyle.Normal),
+    Font(R.font.paperlogy_semi_bold, FontWeight.SemiBold, FontStyle.Normal),
+    Font(R.font.paperlogy_thin, FontWeight.Thin, FontStyle.Normal)
+)
