7dea18e396bc0cfb5c6b57879a2b55ef2ebac761
gmin <gllom7884@gmail.com>
2025-07-18 20:10:47 +0900

    💄 EditButton.kt 수정
    수정 버튼 디자인 코드 완성

--- feature/file/src/main/java/com/example/file/ui/EditButton.kt
+++ feature/file/src/main/java/com/example/file/ui/EditButton.kt
@@ -1,56 +1,43 @@
+// 수정 버튼
+
 package com.example.file.ui
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.file.R
+import com.example.file.ui.theme.DefaultFont
+import com.example.file.ui.theme.White
 
 @Composable
 fun EditButton() {
-    Row(
-        modifier = Modifier
-            .border(
-                width = 0.5.dp,
-                color = Color(0x1A000000),
-                shape = RoundedCornerShape(size = 2.dp)
-            )
-            .width(46.dp)
-            .height(28.dp)
-        .background(color = Color(0x0D000000), shape = RoundedCornerShape(size = 2.dp)),
-        horizontalArrangement = Arrangement.spacedBy(2.dp, Alignment.CenterHorizontally),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Text(
-            modifier = Modifier
-                .width(38.dp)
-                .height(24.dp),
-            text = "EDIT",
-            style = TextStyle(
-                fontSize = 18.sp,
-                lineHeight = 24.sp,
-                //fontFamily = FontFamily(Font(R.font.roboto)),
-                fontWeight = FontWeight(500),
-                color = Color(0xFF000000),
-            )
-        )
-    }
+
+    // 수정 버튼
+    Text(
+        // 텍스트 내용 ("수정")
+        text = "수정",
+
+        // 텍스트 크기 (15sp)
+        fontSize = 15.sp,
+
+        // 한 줄 높이 (22sp)
+        lineHeight = 22.sp,
+
+        // 사용할 폰트 (paperlogy 폰트)
+        fontFamily = DefaultFont,
+
+        // 폰트 굵기 (보통)
+        fontWeight = FontWeight.Normal,
+
+        // 글자색 (White)
+        color = White,
+
+        // 텍스트 정렬 방식 (오른쪽 정렬)
+        textAlign = TextAlign.Right,
+    )
 }
 
 @Preview(showBackground = true)
