7dfd733ec7726b084770226a7387b7557bae0727
gmin <gllom7884@gmail.com>
2025-07-29 23:18:05 +0900

    ✨ 새 파일: LinksGrid.kt
    링크를 2열 그리드로 나타내는 컴포넌트

---
+++ feature/file/src/main/java/com/example/file/ui/content/LinksGrid.kt
@@ -0,0 +1,37 @@
+package com.example.file.ui.content
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.cheonjaeung.compose.grid.SimpleGridCells
+import com.cheonjaeung.compose.grid.VerticalGrid
+import com.example.file.ui.item.LinkItemLayout
+
+@Composable
+fun LinksGrid(
+    linkList: List<String>
+){
+    VerticalGrid(
+        modifier = Modifier
+            .fillMaxWidth(),
+        columns = SimpleGridCells.Fixed(2),
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalArrangement = Arrangement.spacedBy(18.51.dp),
+    ) {
+        // items 람다 안에 folder를 넘겨줘야 FolderItemLayout에서 사용할 수 있어!
+        linkList.forEach { link ->
+            LinkItemLayout(linkList)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LinksGridTest(){
+    LinksGrid(
+        listOf("나의 폴더", "공유받은 폴더")
+    )
+}
