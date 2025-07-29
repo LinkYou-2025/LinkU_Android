9959d492380afd5e3d6d5a679573a3489e70e134
Jihyun <hjh20030615@gmail.com>
2025-07-30 01:47:18 +0900

    :sparkles: 새로운 화면 생성

---
+++ feature/home/src/main/java/com/example/home/component/AIArticleModal.kt
@@ -0,0 +1,15 @@
+package com.example.home.component
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun AIArticleModal() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewAIArticleModal() {
+    AIArticleModal()
+}
