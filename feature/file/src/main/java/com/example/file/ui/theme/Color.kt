f2c998bf4734d07351e72386d2260757e40c518f
gmin <gllom7884@gmail.com>
2025-07-29 22:37:37 +0900

    🔥 코드가 사라진 버그 수정

--- feature/file/src/main/java/com/example/file/ui/theme/Color.kt
+++ feature/file/src/main/java/com/example/file/ui/theme/Color.kt
@@ -43,3 +43,127 @@ val Purple300 = Color(0xFF9A3AB5)
 val Purple200 = Color(0xFFCB59EB)
 val Purple100 = Color(0xFFE5ACF4)
 val Purple50 = Color(0xFFFBEEFF)
+
+data class CategoryColorStyle(
+    val color1: Color,
+    val color2: Color,
+    val color3: Color,
+    val color4: Color,
+) {
+    // verticalGradient: color1 ~ color2로 만드는 함수
+    fun verticalGradient(): Brush = Brush.verticalGradient(
+        colorStops = arrayOf(
+            0.2f to color1.copy(alpha = 0.8f),
+            1.0f to color2.copy(alpha = 0.7f)
+        )
+    )
+
+    // horizontalGradient: color1 ~ color2로 만드는 함수
+    fun horizontalGradient(): Brush = Brush.horizontalGradient(
+        colorStops = arrayOf(
+            0.2f to color1.copy(alpha = 0.8f),
+            1.0f to color2.copy(alpha = 0.7f)
+        )
+    )
+
+    companion object {
+        val categoryStyleList: List<CategoryColorStyle> = listOf(
+            CategoryColorStyle(
+                color1 = Color(0xFFFFEEEE),
+                color2 = Color(0xFFFFC2C2),
+                color3 = Color(0xFFFFA0A0),
+                color4 = Color(0xFFFF5353),
+            ),
+            CategoryColorStyle(
+                color1 = Color(0xFFFFEBE1),
+                color2 = Color(0xFFFFD3C1),
+                color3 = Color(0xFFFFA783),
+                color4 = Color(0xFFFF6A2B),
+            ),
+            CategoryColorStyle(
+                color1 = Color(0xFFFFEFDD),
+                color2 = Color(0xFFFFD4A3),
+                color3 = Color(0xFFFFB867),
+                color4 = Color(0xFFFF9C2B),
+            ),
+            CategoryColorStyle(
+                color1 = Color(0xFFFFFAEB),
+                color2 = Color(0xFFFFEEBE),
+                color3 = Color(0xFFFFE291),
+                color4 = Color(0xFFFFCE45),
+            ),
+            CategoryColorStyle(
+                color1 = Color(0xFFEEFFE0),
+                color2 = Color(0xFFDAFDBD),
+                color3 = Color(0xFFB8F785),
+                color4 = Color(0xFF77E61D),
+            ),
+            CategoryColorStyle(
+                color1 = Color(0xFFDAF7EB),
+                color2 = Color(0xFFB8F0D9),
+                color3 = Color(0xFF6BDFAE),
+                color4 = Color(0xFF00C774),
+            ),
+            CategoryColorStyle(
+                color1 = Color(0xFFC7E8CD),
+                color2 = Color(0xFF9ECCA6),
+                color3 = Color(0xFF70AA79),
+                color4 = Color(0xFF4B9857),
+            ),
+            CategoryColorStyle(
+                color1 = Color(0xFFDDFFFB),
+                color2 = Color(0xFFB9F7F0),
+                color3 = Color(0xFF98EDE2),
+                color4 = Color(0xFF36D1BE),
+            ),
+            CategoryColorStyle(
+                color1 = Color(0xFFECF8FF),
+                color2 = Color(0xFFD0EEFF),
+                color3 = Color(0xFF97D8FF),
+                color4 = Color(0xFF34BBFF),
+            ),
+            CategoryColorStyle(
+                color1 = Color(0xFFE6ECFF),
+                color2 = Color(0xFFC2D2FF),
+                color3 = Color(0xFF82A3FF),
+                color4 = Color(0xFF4C7AF8),
+            ),
+            CategoryColorStyle(
+                color1 = Color(0xFFEDE4FF),
+                color2 = Color(0xFFC5A6FF),
+                color3 = Color(0xFFA778FF),
+                color4 = Color(0xFF813CFF),
+            ),
+            CategoryColorStyle(
+                color1 = Color(0xFFF4E6FF),
+                color2 = Color(0xFFE7C6FF),
+                color3 = Color(0xFFD8A3FF),
+                color4 = Color(0xFFBA5AFF),
+            ),
+            CategoryColorStyle(
+                color1 = Color(0xFFFFEBFB),
+                color2 = Color(0xFFFFC7F5),
+                color3 = Color(0xFFFF9FED),
+                color4 = Color(0xFFFF52DF),
+            ),
+            CategoryColorStyle(
+                color1 = Color(0xFFFFE7F2),
+                color2 = Color(0xFFFFC5E0),
+                color3 = Color(0xFFFFA2CC),
+                color4 = Color(0xFFFF459C),
+            ),
+            CategoryColorStyle(
+                color1 = Color(0xFFF4E6DB),
+                color2 = Color(0xFFE0B795),
+                color3 = Color(0xFFBE9A7B),
+                color4 = Color(0xFF906744),
+            ),
+            CategoryColorStyle(
+                color1 = Color(0xFFD4D4D4),
+                color2 = Color(0xFFAFAFAF),
+                color3 = Color(0xFF747474),
+                color4 = Color(0xFF000000),
+            )
+        )
+    }
+}
