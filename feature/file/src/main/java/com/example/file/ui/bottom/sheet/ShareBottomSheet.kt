919acd4c02a850a57408dfa57e265436d1b1b128
gmin <gllom7884@gmail.com>
2025-07-29 23:15:40 +0900

    ✨ 새 파일: ShareBottomSheet.kt
    공유하기 클릭 시, 나오는 바텀 시트

---
+++ feature/file/src/main/java/com/example/file/ui/bottom/sheet/ShareBottomSheet.kt
@@ -0,0 +1,13 @@
+package com.example.file.ui.bottom.sheet
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ShareBottomSheet(){}
+
+@Preview(showBackground = true)
+@Composable
+fun ShareBottomSheetPreview(){
+    ShareBottomSheet()
+}
