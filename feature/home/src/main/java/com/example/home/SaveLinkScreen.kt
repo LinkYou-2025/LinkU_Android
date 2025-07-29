8607b9fc4dff6568021d8dedb63558b393532beb
Jihyun <hjh20030615@gmail.com>
2025-07-30 01:47:18 +0900

    :sparkles: 새로운 화면 생성

---
+++ feature/home/src/main/java/com/example/home/SaveLinkScreen.kt
@@ -0,0 +1,15 @@
+package com.example.home
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun SaveLinkScreen() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewSaveLinkScreen() {
+    SaveLinkScreen()
+}
