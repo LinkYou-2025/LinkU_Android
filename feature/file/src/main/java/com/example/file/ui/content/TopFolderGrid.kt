f6cb1b8e1f39d742e55736e73b604f2aac171bef
gmin <gllom7884@gmail.com>
2025-07-30 13:09:17 +0900

    💄 폴더, 파일을 나타내는 그리드 UI 코드 정리
    아이템 레이아웃의 변경에 맞춰 수정함

--- feature/file/src/main/java/com/example/file/ui/content/TopFolderGrid.kt
+++ feature/file/src/main/java/com/example/file/ui/content/TopFolderGrid.kt
@@ -8,19 +8,44 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.cheonjaeung.compose.grid.SimpleGridCells
 import com.cheonjaeung.compose.grid.VerticalGrid
+import com.example.file.ui.item.TopFolderItemLayout
+import com.example.file.ui.theme.CategoryColorStyle
+
+val categories = listOf(
+    "어학",
+    "뉴스",
+    "공부법",
+    "IT·개발",
+    "자기계발",
+    "취업·이직",
+    "비즈니스 인사이트",
+    "생산성·툴",
+    "라이프스타일",
+    "심리·자기이해",
+    "에세이·칼럼",
+    "트렌드",
+    "디자인·예술",
+    "영상·뮤직",
+    "맛집·여행",
+    "기타"
+)
 
 @Composable
 fun TopFolderGrid(
-    folderList: List<String>
 ){
     VerticalGrid(
         modifier = Modifier
             .fillMaxWidth(),
         columns = SimpleGridCells.Fixed(2),
-        horizontalArrangement = Arrangement.SpaceEvenly,
+        horizontalArrangement = Arrangement.spacedBy(20.dp),
         verticalArrangement = Arrangement.spacedBy(18.51.dp),
     ) {
-        folderList.forEach { folder ->
+        for (i in 0..15) {
+            TopFolderItemLayout(
+                categoryColorStyle = CategoryColorStyle.categoryStyleList[i],
+                categoryName = categories[i],
+                isBookmarked = false
+            )
         }
     }
 }
@@ -29,6 +54,5 @@ fun TopFolderGrid(
 @Composable
 fun TopFolderGridTest(){
     TopFolderGrid(
-        listOf("나의 폴더", "공유받은 폴더")
     )
 }
