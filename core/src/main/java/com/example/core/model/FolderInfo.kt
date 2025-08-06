0307000dfef79059377123770e33887e27a4e0d2
gmin <gllom7884@gmail.com>
2025-08-06 15:56:39 +0900

    ✨ 새 파일: FolderInfo.kt
    폴더의 자세한 정보를 담은 구조

---
+++ core/src/main/java/com/example/core/model/FolderInfo.kt
@@ -0,0 +1,13 @@
+package com.example.core.model
+
+import java.time.OffsetDateTime
+
+data class FolderInfo(
+    val folderId: Long,
+    val folderName: String,
+    val categoryId: Long,
+    val categoryName: String,
+    val parentFolderId: Long,
+    val createdAt: OffsetDateTime,
+    val updatedAt: OffsetDateTime
+)
