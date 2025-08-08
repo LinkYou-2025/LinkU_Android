7b8e067ccfb7e1c2343f7e68720d690c22fd2b7b
Yunzi <katete051@naver.com>
2025-08-08 15:57:21 +0900

    import 문 오류 수정

--- feature/curation/src/main/java/com/example/curation/CurationLikedSection.kt
+++ feature/curation/src/main/java/com/example/curation/CurationLikedSection.kt
@@ -30,6 +30,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.curation.R
 import com.example.curation.Paperlogy
 
 @Composable
