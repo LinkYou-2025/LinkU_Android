d711dc21a2475948125130e65b343bd834daa81e
gmin <gllom7884@gmail.com>
2025-08-10 15:48:45 +0900

    ✨ 소분류 이름 변경 구현

--- core/src/main/java/com/example/core/model/FolderSimpleInfo.kt
+++ core/src/main/java/com/example/core/model/FolderSimpleInfo.kt
@@ -5,7 +5,7 @@ import java.time.OffsetDateTime
 // com.example.data.api.dto.server.FolderListResponseDTO -> 구조를 공유
 data class FolderSimpleInfo(
     val folderId: Long,
-    var folderName: String,
+    val folderName: String,
     val parentFolderId: Long,
     val isBookmarked: Boolean
 )
--- feature/file/src/main/java/com/example/file/ui/bottom/sheet/BottomFolderEditBottomSheet.kt
+++ feature/file/src/main/java/com/example/file/ui/bottom/sheet/BottomFolderEditBottomSheet.kt
@@ -1,10 +1,12 @@
 package com.example.file.ui.bottom.sheet
 
 import androidx.compose.runtime.Composable
+import com.example.file.FileViewModel
 import com.example.file.viewmodel.folder.state.FolderStateViewModel
 
 @Composable
 fun BottomFolderEditBottomSheet(
+    onTextDeliver: (String) -> Unit,
     folderStateViewModel: FolderStateViewModel
 ){
     TextFieldFileBottomSheet(
@@ -12,6 +14,7 @@ fun BottomFolderEditBottomSheet(
         body = "변경할 폴더명을 입력해주세요!",
         placeholderText = folderStateViewModel.selectedBottomFolder?.folderName?:"",
         visible = folderStateViewModel.bottomFolderEditBottomSheetVisible,
+        onTextDeliver = { onTextDeliver(it) },
         onDismiss = { folderStateViewModel.updateBottomFolderEditBottomSheetVisible(false) }
     )
 }
