7ab944f4e5c1999854779aac3fa4b0592a77bb8e
Yunzi <katete051@naver.com>
2025-08-15 23:19:50 +0900

    🐛 큐레이션 api 연동 수정

--- data/src/main/java/com/example/data/api/dto/server/CurationLatestResponse.kt
+++ data/src/main/java/com/example/data/api/dto/server/CurationLatestResponse.kt
@@ -1,16 +1,11 @@
 package com.example.data.api.dto.server
 
+import com.squareup.moshi.JsonClass
 import com.squareup.moshi.Json
 
+@JsonClass(generateAdapter = true)
 data class CurationLatestResponse(
-
-    @Json(name = "curationId")
-    val curationId: Long,
-
-    @Json(name = "month")
-    val month: String,
-
-    @Json(name = "thumbnailUrl")
-    val thumbnailUrl: String
-
+    @Json(name = "curationId") val curationId: Long,
+    @Json(name = "month") val month: String,
+    @Json(name = "thumbnailUrl") val thumbnailUrl: String
 )
