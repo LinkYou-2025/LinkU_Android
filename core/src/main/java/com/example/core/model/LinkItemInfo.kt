2353501f6478a95a44fcbf701bd37237dd5cae13
gmin <gllom7884@gmail.com>
2025-08-17 01:16:57 +0900

    ✨ 새 파일: LinkItemInfo.kt
    파일 항목에서 사용하는 링크 정보 구조

---
+++ core/src/main/java/com/example/core/model/LinkItemInfo.kt
@@ -0,0 +1,13 @@
+package com.example.core.model
+
+import java.time.OffsetDateTime
+
+data class LinkItemInfo(
+    val linkuId: Long,
+    val parentFolderId: Long,
+    val title: String,
+    val url: String,
+    val tags: List<String> = emptyList(),
+    val linkuImageUrl: String?,
+    val createdAt: OffsetDateTime?,
+)
