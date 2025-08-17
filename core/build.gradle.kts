0f3aa04c84de2843d6c8c188d9f6ee01114d5d73
gmin <gllom7884@gmail.com>
2025-08-17 14:47:29 +0900

    💄 중복 코드 제거
    implementation(project(":design")) -> 두 줄이었음

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -39,8 +39,6 @@ dependencies {
 
     // design 모듈 의존성
     implementation(project(":design"))
-    implementation(project(":design"))
-
 
 
     // ✅ Retrofit2 & OkHttp 추가 (버전은 app 모듈과 동일한 libs 사용)
