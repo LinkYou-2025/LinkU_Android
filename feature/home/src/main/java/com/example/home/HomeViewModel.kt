6bc07b547246fa36d4edc06c939c0cd7383dc359
gmin <gllom7884@gmail.com>
2025-08-21 16:42:19 +0900

    🔥 파일 내 링크 클릭 시 상세정보 안 나오는 버그 해결

--- feature/home/src/main/java/com/example/home/HomeViewModel.kt
+++ feature/home/src/main/java/com/example/home/HomeViewModel.kt
@@ -16,9 +16,9 @@ import com.example.core.repository.LinkuRepository
 import com.example.core.repository.RecentSearchRepository
 import com.example.core.repository.UserRepository
 import com.example.data.preference.AuthPreference
-import com.example.file.ui.theme.CategoryColorStyle
-import com.example.file.ui.theme.toCategoryColorStyleMap
+import com.example.data.util.toCategoryColorStyleMap
 import com.example.design.FastSearchItem
+import com.example.design.theme.color.CategoryColorStyle
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
@@ -26,8 +26,6 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import retrofit2.HttpException
