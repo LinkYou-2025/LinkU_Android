60c43454c6141b14c23803bd6663b18f0732b3ec
gmin <gllom7884@gmail.com>
2025-09-30 21:28:03 +0900

    💡 내비게이션 바 관련 코드 리팩토링
    주석 추가 및 코드 정리

--- design/src/main/java/com/example/design/modifier/GradientTint.kt
+++ design/src/main/java/com/example/design/modifier/GradientTint.kt
@@ -10,7 +10,7 @@ import androidx.compose.ui.graphics.graphicsLayer
 * 그라데이션 색상을 적용하는 Modifier 확장 함수
 * 기존의 이미지 위에 블렌드 모드에 따라 색을 덮는 방식
 * */
-fun Modifier.gradiantTint(
+fun Modifier.gradientTint(
     /*
     * brush: 그라데이션 색상을 적용할 브러시
     * blendMode: 그라데이션 색상을 적용할 때 사용할 블렌드 모드
