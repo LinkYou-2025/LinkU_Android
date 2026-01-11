3a3af83fbc735e8a77d6296dc21c9f9d28b82992
gmin <gllom7884@gmail.com>
2026-01-11 21:13:25 +0900

    ✨ `BottomFolderEditBottomSheet`에 `sheetState` 매개변수 추가 및 설정 최적화
    - `BottomFolderEditBottomSheet`에서 `TextFieldFileBottomSheet` 호출 시 `sheetState` 인자를 추가하여 상태 관리 개선
    - `rememberModalBottomSheetState`에 `skipPartiallyExpanded = true` 옵션을 적용하여 바텀 시트가 항상 전체 확장 상태로 열리도록 수정
    - 불필요한 `FileViewModel` 임포트 제거

--- feature/file/src/main/java/com/example/file/ui/bottom/sheet/BottomFolderEditBottomSheet.kt
+++ feature/file/src/main/java/com/example/file/ui/bottom/sheet/BottomFolderEditBottomSheet.kt
@@ -1,8 +1,8 @@
 package com.example.file.ui.bottom.sheet
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
-import com.example.file.FileViewModel
 import com.example.file.viewmodel.folder.state.FolderStateViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -16,6 +16,9 @@ fun BottomFolderEditBottomSheet(
         body = "변경할 폴더명을 입력해주세요!",
         placeholderText = folderStateViewModel.readyToUpdateBottomFolder?.folderName?:"에러",
         visible = folderStateViewModel.bottomFolderEditBottomSheetVisible,
+        sheetState = rememberModalBottomSheetState(
+            skipPartiallyExpanded = true
+        ),
         onTextDeliver = { onTextDeliver(it) },
         onDismiss = { folderStateViewModel.updateBottomFolderEditBottomSheetVisible(false) }
     )
