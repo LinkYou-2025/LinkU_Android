188d3bbd24b49835ef9a4f500a35de5b18ebe715
Jihyun <hjh20030615@gmail.com>
2026-02-03 20:11:32 +0900

    :sparkles: 알림함 관련 코드 1차 생성

---
+++ feature/home/src/main/java/com/example/home/ui/alarm/top/bar/component/AlarmTopHeader.kt
@@ -0,0 +1,63 @@
+package com.example.home.ui.alarm.top.bar.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.design.theme.LocalColorTheme
+import com.example.design.theme.LocalFontTheme
+import com.example.home.R
+
+
+@Composable
+fun AlarmTopHeader() {
+    Box(
+        modifier = Modifier.fillMaxWidth(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "알림함",
+            color = LocalColorTheme.current.black,
+            fontSize = 16.sp,
+            fontFamily = LocalFontTheme.current.font,
+            fontWeight = FontWeight.Medium
+        )
+
+        Row(
+            modifier = Modifier.align(Alignment.CenterStart),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(R.drawable.ic_back),
+                contentDescription = null,
+                modifier = Modifier.width(11.dp)
+            )
+
+            Spacer(modifier = Modifier.width(23.dp))
+
+            Image(
+                painter = painterResource(R.drawable.ic_home),
+                contentDescription = null,
+                modifier = Modifier.size(22.dp)
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewAlarmTopHeader() {
+    AlarmTopHeader()
+}
