de22a2b4342c949c80888563212e46ff0e636b64
gmin <gllom7884@gmail.com>
2026-03-12 14:44:23 +0900

    ✨ feature/file: 그리드 컴포넌트 내 가이드 주석 추가 및 UI 레이아웃 임시 수정
    - `LinksGrid`, `TopFolderGrid`, `BottomFolderGrid` 등 주요 그리드 컴포넌트에 `VerticalGrid` 사용 관련 기술 부채 및 `LazyVerticalGrid` 전환 예정 안내 주석 추가
    - `TopFolderGrid` 내 아이템 레이아웃의 고정 높이(`153.52dp`) 임시 적용
    - `TopFolderItemLayout`의 modifier를 `fillMaxSize`에서 `fillMaxHeight`로 변경하여 레이아웃 정렬 수정

--- feature/file/src/main/java/com/example/file/ui/content/TopFolderGrid.kt
+++ feature/file/src/main/java/com/example/file/ui/content/TopFolderGrid.kt
@@ -3,8 +3,10 @@ package com.example.file.ui.content
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
@@ -30,6 +32,14 @@ fun TopFolderGrid(
     editStateViewModel: EditStateViewModel,
 ){
     val folderList = fileViewModel.parentFolders.collectAsState().value
+
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    * 스크롤은 compose 전 자식 요소에게 크기를 물어보고 최종 크기를 결정하는 방식.   *
+    * SequentialGrid 기반인 VerticalGrid는 SubcomposeLayout임.                       *
+    * 이는 자식에게 묻는 과정에서 compose가 다시 발생하는 문제로 최신부턴 사용 불가. *
+    * 그러므로 꼭 LazyVertical 그리드로 변경 예정.                                   *
+    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
     VerticalGrid(
         modifier = Modifier
             .fillMaxWidth(),
@@ -43,6 +53,8 @@ fun TopFolderGrid(
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
+                    // 임시 추가
+                    .height(153.52942.dp)
                     .noRippleClickable {
                         if (editStateViewModel.isEditMode) {
                             folderStateViewModel.updateReadyToUpdateTopFolder(folder)
@@ -57,7 +69,8 @@ fun TopFolderGrid(
                 verticalAlignment = Alignment.CenterVertically
             ){
                 TopFolderItemLayout(
-                    modifier = Modifier.fillMaxSize(164f/174f),
+                    //modifier = Modifier.fillMaxSize(164f/174f),   // 임시 제거
+                    modifier = Modifier.fillMaxHeight(164f/174f),   // 임시 추가
                     colorStyle = categoryColorStyle?:CategoryColorStyle.categoryStyleList[0],
                     folderName = folder.folderName,
                     isBookmarked = folder.isBookmarked,
