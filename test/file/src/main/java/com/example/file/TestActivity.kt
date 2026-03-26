817975ee20361e476437368130868165eaff3886
gmin <gllom7884@gmail.com>
2026-03-26 17:07:07 +0900

    ✨ `TestActivity` 내 `ThemeProvider` 적용 및 UI 테마 설정 추가
    - `setContent` 블록 내 `FileApp`을 `ThemeProvider`로 감싸 전체 UI에 테마 스타일이 적용되도록 수정
    - 테마 적용을 위한 `ThemeProvider` import 추가

--- test/file/src/main/java/com/example/file/TestActivity.kt
+++ test/file/src/main/java/com/example/file/TestActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.example.design.theme.ThemeProvider
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -12,7 +13,8 @@ class TestActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            FileApp()
+
+            ThemeProvider { FileApp() }
         }
     }
 }
