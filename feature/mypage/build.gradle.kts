504192a0cfde3473f6112157181e7b7f29ba3d0b
Jihyun <hjh20030615@gmail.com>
2026-03-26 16:26:16 +0900

    :lipstick: FAQ 화면 완료

--- feature/mypage/build.gradle.kts
+++ feature/mypage/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation(libs.androidx.material3)
 //    implementation(libs.androidx.navigation.compose.jvmstubs)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.foundation.layout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
