809e043269158ec81ca1873cf4c3b16357bb8b2b
Chea-yunzi <katete051@naver.com>
2026-05-12 17:16:45 +0900

    ✨ refactor(data): 네트워크 가용성 확인 로직 강화
    - `isNetworkAvailable` 함수에서 `NET_CAPABILITY_INTERNET`뿐만 아니라 `NET_CAPABILITY_VALIDATED` 권한까지 확인하도록 로직을 수정하여 실제 인터넷 연결 여부를 더 정확하게 판단하도록 개선

--- data/src/main/java/com/linku/data/network/NetworkReceiver.kt
+++ data/src/main/java/com/linku/data/network/NetworkReceiver.kt
@@ -68,6 +68,7 @@ class NetworkReceiver @Inject constructor(
     private fun isNetworkAvailable(): Boolean {
         val network = connectivityManager.activeNetwork ?: return false
         val capabilities = connectivityManager.getNetworkCapabilities(network) ?: return false
-        return capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+        return capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) &&
+                capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
     }
 }
