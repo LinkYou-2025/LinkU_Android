018f6602c8722e43f52ad04bb56421136d0cd2b5
Chea-yunzi <katete051@naver.com>
2026-06-10 16:57:29 +0900

    ✨ feat(login): 비밀번호 재설정 화면용 Side Effect 정의 (`ResetPasswordEffect`)
    - 비밀번호 재설정 로직의 일회성 부수 효과를 관리하는 `ResetPasswordEffect` sealed interface 추가
    - 메일 발송 성공 시 화면 전환을 위한 `NavigateToEmailLogin` 정의
    - 메일 발송 실패 시 에러 처리를 위한 `ShowError` 정의

--- feature/login/src/main/java/com/linku/login/viewmodel/state/PasswordStepState.kt
+++ feature/login/src/main/java/com/linku/login/viewmodel/state/PasswordStepState.kt
@@ -8,14 +8,13 @@ package com.linku.login.viewmodel.state
  * @property doPasswordsMatch 기본 비밀번호와 비밀번호 확인창의 입력값 일치 여부. (불일치 경고 노출용)
  * @property isPasswordValid 길이와 복잡도 조건을 모두 통과했는지 여부. (true일 때 확인창 슬롯 노출)
  * @property canProceed 모든 조건을 만족하여 다음 단계로 진행 가능한지 여부. (버튼 활성화 제어)
- * @property isNavigateTrigger 다음 가입 단계(닉네임 설정)로 이동하기 위한 일회성 화면 전환 트리거.
  */
 
 internal data class PasswordStepState(
+    val confirmPassword: String = "",
     val isLengthValid: Boolean = false,
     val isComplex: Boolean = false,
     val doPasswordsMatch: Boolean = false,
     val isPasswordValid: Boolean = false,
     val canProceed: Boolean = false,
-    val isNavigateTrigger: Boolean = false
 )
