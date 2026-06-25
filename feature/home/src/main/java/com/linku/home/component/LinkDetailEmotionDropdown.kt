91512d1faa5ba573a8502826bc8cef5a30223465
Jihyun <hjh20030615@gmail.com>
2026-06-25 20:22:25 +0900

    :zap: core에 있는 EmotionType을 공통적으로 사용하게 수정

--- feature/home/src/main/java/com/linku/home/component/LinkDetailEmotionDropdown.kt
+++ feature/home/src/main/java/com/linku/home/component/LinkDetailEmotionDropdown.kt
@@ -24,7 +24,6 @@ import com.linku.core.model.EmotionType
 import com.linku.design.modifier.noRippleClickable
 import com.linku.design.theme.ThemeProvider
 import com.linku.design.theme.linkuColors
-import com.linku.home.R
 import com.linku.home.util.imgRes
 
 @Composable
@@ -76,17 +75,6 @@ fun LinkDetailEmotionDropdown(
                 )
             }
         }
-    }
-}
-
-private fun EmotionType.iconRes(): Int {
-    return when (this) {
-        EmotionType.JOY -> R.drawable.ic_joy
-        EmotionType.CALM -> R.drawable.ic_calm
-        EmotionType.EXCITE -> R.drawable.ic_excite
-        EmotionType.SAD -> R.drawable.ic_sad
-        EmotionType.IRRITATION -> R.drawable.ic_irritation
-        EmotionType.ANGER -> R.drawable.ic_anger
     }
 }
 
