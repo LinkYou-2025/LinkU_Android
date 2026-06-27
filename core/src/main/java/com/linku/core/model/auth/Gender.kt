1eff763f8bdbfab23345b45ee86d38ccf7451e86
Chea-yunzi <katete051@naver.com>
2026-06-27 16:45:13 +0900

    ♻️ refactor(core): `Gender.kt` 내 팩토리 메서드 파라미터 명칭 수정
    - `Gender.fromValue` 메서드의 파라미터명을 `value`에서 `apiValue`로 변경하여 서버 응답 값임을 명확히 함

--- core/src/main/java/com/linku/core/model/auth/Gender.kt
+++ core/src/main/java/com/linku/core/model/auth/Gender.kt
@@ -7,7 +7,7 @@ enum class Gender(val value: String) {
 
     // 서버에서 받은 String을 이넘으로 받아오는 로직, 마이페이지에서 사용할 수 있기에 추가함.
     companion object {
-        fun fromValue(value: String?): Gender =
-            entries.find { it.value.equals(value, ignoreCase = true) } ?: NONE
+        fun fromValue(apiValue: String?): Gender =
+            entries.find { it.value.equals(apiValue, ignoreCase = true) } ?: NONE
     }
 }
