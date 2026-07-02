e1670f09138cb1b4b592bd26cd57de64a28861d1
princehw03 <princehw03@gmail.com>
2026-07-02 18:32:44 +0900

    resolve conflict

--- feature/home/src/main/java/com/linku/home/HomeApp.kt
+++ feature/home/src/main/java/com/linku/home/HomeApp.kt
@@ -6,6 +6,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.platform.LocalContext
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
