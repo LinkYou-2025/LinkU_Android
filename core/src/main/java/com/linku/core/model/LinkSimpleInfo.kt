63d3e6724075d305761142314fa2b6c902682b7c
Jihyun <hjh20030615@gmail.com>
2026-07-07 18:05:13 +0900

    :art: 링크 생성 도메인 모델 및 DTO 수정

--- core/src/main/java/com/linku/core/model/LinkSimpleInfo.kt
+++ core/src/main/java/com/linku/core/model/LinkSimpleInfo.kt
@@ -5,7 +5,8 @@ data class LinkSimpleInfo(
     val categoryId: Long?,
     val memo: String?,
     val emotionId: Long?,
-    val title: String,
+    val situationId: Long? = null,  // 충돌 방지를 위해 기본값 추가, merge 한 뒤에 기본값 제거 예정
+    val title: String = "",  // 충돌 방지를 위해 기본값 추가, merge 한 뒤에 기본값 제거 예정
     val domain: String,
     val domainImageUrl: String?,
     val linkuImageUrl: String?,
