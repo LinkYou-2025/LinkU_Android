44b46a0ca48b2f08014ed246f8032ccdad8d9336
Jihyun <hjh20030615@gmail.com>
2026-07-07 18:05:30 +0900

    :sparkles: 링크 유효성 검사 도메인 모델 및 DTO 생성

---
+++ data/src/main/java/com/linku/data/api/dto/server/LinkCheckDTO.kt
@@ -0,0 +1,29 @@
+package com.linku.data.api.dto.server
+
+import com.google.gson.annotations.SerializedName
+
+data class LinkCheckDTO(
+    @SerializedName("userId")
+    val userId: Long?,
+
+    @SerializedName("linkuId")
+    val linkuId: Long?,
+
+    @SerializedName("title")
+    val title: String?,
+
+    @SerializedName("memo")
+    val memo: String?,
+
+    @SerializedName("emotionId")
+    val emotionId: Long?,
+
+    @SerializedName("createdAt")
+    val createdAt: String?,
+
+    @SerializedName("updatedAt")
+    val updatedAt: String?,
+
+    @SerializedName("exist")
+    val exist: Boolean?
+)
