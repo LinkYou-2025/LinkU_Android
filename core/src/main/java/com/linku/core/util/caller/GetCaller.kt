e07c0ecb3dcc7d61b7dba37efb44d71ec2394ba9
gmin <gllom7884@gmail.com>
2026-07-18 05:24:41 +0900

    💡 docs(util): `getCaller` 함수에 KDoc 추가
    - **`getCaller()` 함수에 대한 상세 설명 주석 추가**
        - 함수가 수행하는 역할(상위 메서드의 `StackTraceElement` 정보 반환) 명시
        - 스택 트레이스의 특정 인덱스(1번)를 참조하여 호출 지점 정보를 가져오는 동작 방식 기술
        - 반환 타입에 대한 설명 추가 (KDoc `@return` 적용)

--- core/src/main/java/com/linku/core/util/caller/GetCaller.kt
+++ core/src/main/java/com/linku/core/util/caller/GetCaller.kt
@@ -1,3 +1,11 @@
 package com.linku.core.util.caller
 
+/**
+ * 이 함수를 호출한 상위 메서드의 [StackTraceElement] 정보를 가져옵니다.
+ *
+ * 스택 트레이스의 인덱스 1에 해당하는 요소를 참조하여 이 함수를 직접적으로
+ * 호출한 지점의 클래스명, 메서드명, 파일명 및 라인 번호 정보를 반환합니다.
+ *
+ * @return 호출자의 정보를 담고 있는 [StackTraceElement] 객체
+ */
 fun getCaller(): StackTraceElement = Throwable().stackTrace[1]
