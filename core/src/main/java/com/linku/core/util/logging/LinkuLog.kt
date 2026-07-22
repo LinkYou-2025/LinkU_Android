0a1c1c32796eb46c0c1676bfd09fd48e56165fc0
gmin <gllom7884@gmail.com>
2026-07-22 16:04:59 +0900

    🎨 refactor(logging): 로그 메시지 출력 포맷 수정
    - `LinkuLog` 내 `StackTraceElement.getMsg` 확장 함수의 문자열 템플릿 수정
    - 로그 출력 시 메서드 이름(`methodName`)과 메시지 사이의 콜론(`:`)을 제거하여 포맷 변경

--- core/src/main/java/com/linku/core/util/logging/LinkuLog.kt
+++ core/src/main/java/com/linku/core/util/logging/LinkuLog.kt
@@ -15,5 +15,5 @@ object LinkuLog {
         get() = fileName
 
     @PublishedApi
-    internal inline fun StackTraceElement.getMsg(msg: () -> String) = "${methodName}: ${msg()}"
+    internal inline fun StackTraceElement.getMsg(msg: () -> String) = "$methodName ${msg()}"
 }
