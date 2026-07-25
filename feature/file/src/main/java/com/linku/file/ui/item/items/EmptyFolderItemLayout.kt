5bf6101ace6acb22da83a051829b1f59c2ad32d6
gmin <gllom7884@gmail.com>
2026-07-25 17:07:13 +0900

    🎨 style(ui): 기본 폴더 마스크 그라데이션 통일

--- feature/file/src/main/java/com/linku/file/ui/item/items/EmptyFolderItemLayout.kt
+++ feature/file/src/main/java/com/linku/file/ui/item/items/EmptyFolderItemLayout.kt
@@ -3,7 +3,6 @@ package com.linku.file.ui.item.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import com.linku.design.theme.color.CategoryColorStyle
 import com.linku.design.theme.linkuColors
 import com.linku.file.ui.item.FolderItemLayout
@@ -13,20 +12,14 @@ fun EmptyFolderItemLayout(
     modifier: Modifier = Modifier,
     folderName: String = ""
 ) {
-    val colors = MaterialTheme.linkuColors
     val color = CategoryColorStyle.DEFAULT
 
     FolderItemLayout(
         backgroundColor = color.color2,
         color1 = color.color3,
         color2 = color.color2,
-        color3 = colors.white,
-        folderMaskBrush = Brush.verticalGradient(
-            colorStops = arrayOf(
-                1.0f to colors.gray[100].copy(alpha = 0.7f),
-                0.2f to colors.gray[200].copy(alpha = 1.0f),
-            )
-        ),
+        color3 = MaterialTheme.linkuColors.white,
+        folderMaskBrush = color.verticalGradient(),
         leftIcon = {},
         rightIcon = {},
         textBackgroundColor = color.color4,
