01b5904c50f12f2ac9fc25b0d85f7bbcd2e5e8ea
princehw03 <princehw03@gmail.com>
2026-08-11 17:05:21 +0900

    fix: 충돌 해결 - 각 모듈 테스트 파일 패키지명 수정

--- feature/file/src/test/java/com/linku/file/ExampleUnitTest.kt
+++ feature/file/src/test/java/com/linku/file/ExampleUnitTest.kt
@@ -1,8 +1,4 @@
-<<<<<<<< HEAD:feature/file/src/test/java/com/linku/file/ExampleUnitTest.kt
 package com.linku.file
-========
-package com.linku.data
->>>>>>>> fd1304faab6b86e04c17e31a0786ce151290d292:data/src/test/java/com/linku/data/ExampleUnitTest.kt
 
 import org.junit.Test
 
--- feature/home/src/androidTest/java/com/linku/home/ExampleInstrumentedTest.kt
+++ feature/home/src/androidTest/java/com/linku/home/ExampleInstrumentedTest.kt
@@ -1,8 +1,4 @@
-<<<<<<<< HEAD:feature/home/src/androidTest/java/com/linku/home/ExampleInstrumentedTest.kt
 package com.linku.home
-========
-package com.linku.link
->>>>>>>> fd1304faab6b86e04c17e31a0786ce151290d292:app/src/androidTest/java/com/linku/link/ExampleInstrumentedTest.kt
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -23,10 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-<<<<<<<< HEAD:feature/home/src/androidTest/java/com/linku/home/ExampleInstrumentedTest.kt
         assertEquals("com.linku.home.test", appContext.packageName)
-========
-        assertEquals("com.linku.link", appContext.packageName)
->>>>>>>> fd1304faab6b86e04c17e31a0786ce151290d292:app/src/androidTest/java/com/linku/link/ExampleInstrumentedTest.kt
     }
 }
