1d1ca9779df9887401cd3cb6ed987efbe72a23fb
gmin <gllom7884@gmail.com>
2026-08-27 18:38:48 +0900

    ✅ test(android): 테스트 앱 패키지 검증 정렬

--- test/mypage/src/androidTest/java/com/linku/mypage/ExampleInstrumentedTest.kt
+++ test/mypage/src/androidTest/java/com/linku/mypage/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.linku.mypage.test", appContext.packageName)
+        assertEquals("com.linku.linku.test.mypage", appContext.packageName)
     }
 }
